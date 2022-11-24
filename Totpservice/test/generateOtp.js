7b0eb2100623d65af4827f278ffb9c62f54605ae
MANOJ KUMAR <Manojgum@gmail.com>
2022-11-24 11:24:37 +0530

    Update generateOtp.js

--- Totpservice/test/generateOtp.js
+++ Totpservice/test/generateOtp.js
@@ -20,26 +20,46 @@ describe('.....Aspect check...',function(){
         assert.typeOf(email,'string')
     })
 
-//     it('it post generate otp',function(done){
-//         post('/api/v16.17.0/otp/generateOTP')
-//   .send({ userName: 'manoj kumar',  email:'manojgum@gmail.com', })
-//         .end((err,res)=>{
-//             expect(res).to.have.status(200);
-//             done();
-//         })
-//     })
-
 // generateOtp
 describe('POST /api/v16.17.0/otp/generateOTP',()=>{
-    it("It should POST all the requirement",(done)=>{
+    // it("It should POST all the requirement",(done)=>{
+    //     chai.request(server)
+    //     .post("/api/v16.17.0/otp/generateOTP")
+    //     .end((err,response)=>{
+    //         response.should.have.status(200);
+    //         response.body.should.be.a['array'];
+    //         response.body.length.should.be.eq(3);
+    //         done();
+    //     });
+    // });
+
+    it("It should POST all the error",(done)=>{
         chai.request(server)
         .post("/api/v16.17.0/otp/generateOTP")
         .end((err,response)=>{
-            response.should.have.status(200);
-            response.body.should.be.a['array'];
+            response.should.have.status(401);
             done();
+        });
+    });
+
+it("It should POST all the requirement",(done)=>{
+        chai.request(server)
+        const task={
+            userName:"Manoj kumar",
+            email:"manojgum@gmail.com",
+        }
+        .post("/api/v16.17.0/otp/generateOTP")
+        .send(task)
+        .end((err,response)=>{
+            response.should.have.status(200)
+            response.body.should.be.a(`object`)
+            response.body.should.have.property('userName').eq("Manoj kumar");
+            response.body.should.have.property('email').eq('manojgum@gmail.com')
+            done()
         })
     })
+
+
 })
 
 
@@ -66,11 +86,11 @@ describe('POST /api/v16.17.0/otp/generateOTP',()=>{
 
 
 
-var agent = chai.request.agent()
-agent
-  .post('/api/v16.17.0/otp/generateOTP')
-  .send({ username: 'me', password: '123' })
-  .then(function (res) {
-    expect(res).to.have.cookie('sessionid');
-         expect(res).to.have.status(200);
-  });
+// var agent = chai.request.agent()
+// agent
+//   .post('/api/v16.17.0/otp/generateOTP')
+//   .send({ username: 'me', password: '123' })
+//   .then(function (res) {
+//     expect(res).to.have.cookie('sessionid');
+//          expect(res).to.have.status(200);
+//   });
