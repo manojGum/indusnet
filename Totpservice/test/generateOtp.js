cf33a72778c27abdf243a2f2a5e4897dee670c7e
MANOJ KUMAR <Manojgum@gmail.com>
2022-11-24 15:04:09 +0530

    unit testing

--- Totpservice/test/generateOtp.js
+++ Totpservice/test/generateOtp.js
@@ -45,12 +45,6 @@ describe('POST /api/v16.17.0/otp/generateOTP',()=>{
 it("It should POST all the requirement",(done)=>{
 
         chai.request(server)
-        // const task={
-        //     userName:"manoj kumar",
-        //     email:"manojgum@gmail.com",
-        // }
-        // const  trackId=Math.floor(new Date())
-        // const timestamp=new Date()
         .post("/api/v16.17.0/otp/generateOTP")
         .type('form')
         .send({
@@ -63,25 +57,47 @@ it("It should POST all the requirement",(done)=>{
             response.body.should.have.property('Email').eq(email);
             response.body.should.have.property('token');
             response.body.should.have.property('success').eq(true);
-            response.body.should.have.property('message');
+            response.body.should.have.property('message').eq("OTP  successfully  send in your Email, it will expire in 5 minutes");
             response.body.should.have.property('otp');
+            response.body.should.have.property("timestamp")
             done()
         });
     });
 
 
-    // it("It should NOT POST a new task without the email propertiy",(done)=>{
-    //     chai.request(server)
-    //     const task={
-    //         userName:"manoj kumar"
-    //     }
-    //     .post("/api/v16.17.0/otp/generateOTP")
-    //     .end((err,response)=>{
-    //         response.should.have.status(401)
-    //         response.body.should.be.eq("Email not provided")
-    //         done()
-    //     });
-    // });
+    it("It should NOT POST a new task without the email propertiy",(done)=>{
+        chai.request(server)
+        .post("/api/v16.17.0/otp/generateOTP")
+        .type('form')
+        .send({
+            userName:"manoj kumar"
+        })
+        .end((err,response)=>{
+            response.should.have.status(401)
+            response.body.should.have.property("message").eq("Email not provided")
+            response.body.should.have.property("timestamp")
+            response.body.should.have.property("trackId")
+            done()
+        });
+    });
+
+
+    it("It should  the email propertiy is not proper ",(done)=>{
+        chai.request(server)
+        .post("/api/v16.17.0/otp/generateOTP")
+        .type('form')
+        .send({
+            userName:"manoj kumar",
+            email:"manojgum"
+        })
+        .end((err,response)=>{
+            response.should.have.status(400)
+            response.body.should.have.property("message").eq("Enter valid Email")
+            response.body.should.have.property("timestamp")
+            response.body.should.have.property("trackId")
+            done()
+        });
+    });
 
 
 
