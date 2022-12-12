9ad06cf896c6ebaec2afe67390e57aee979b18d4
MANOJ KUMAR <Manojgum@gmail.com>
2022-12-12 18:21:42 +0530

    email configuration
    email configuration

--- Totpservice/src/configs/mail.js
+++ Totpservice/src/configs/mail.js
@@ -13,11 +13,10 @@ const nodemailer = require("nodemailer");
 
 //   module.exports transporter
 module.exports= nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 587,
+    service: "gmail",
     secure: false, // true for 465, false for other ports
     auth: {
-      user: "75f72ecb947a2d", // generated ethereal user
-      pass: "5a01a36b4742f2", // generated ethereal password
+      user: "manojgum@gmail.com", // generated ethereal user
+      pass: "lnrsvfdmvgovyjyg", // generated ethereal password
     },
   });
--- Totpservice/src/controllers/generateOtpController.js
+++ Totpservice/src/controllers/generateOtpController.js
@@ -5,45 +5,54 @@ const Generate = require("../models/generateOtpModel");
 const Isemail = require('isemail');
 const router = express.Router();
 const transporter = require("../configs/mail")
-const otpGenerator = require("otp-generator");
-const crypto       = require("crypto");
+// const otpGenerator = require("otp-generator");
+const crypto  = require("crypto");
 require('dotenv').config()
 const loginAccountLimiter=require("../middlewares/loginAccountLimiter")
 
-
-
-
-
 // Generate a 6 digit numeric OTP
-const createNewOTP=(email)=>{
-    const otp      = otpGenerator.generate(6, {lowerCaseAlphabets:false, upperCaseAlphabets:false,specialChars:false});
+ function generateOTP() {
+        var digits = '0123456789';
+        let OTP = '';
+        for (let i = 0; i < 6; i++ ) {
+            OTP += digits[Math.floor(Math.random() * 10)];
+        }
+        return OTP;
+    }
+
+
+
+const createNewOTP=async(email)=>{
+   
+      
+    const otp= await generateOTP() 
     const ttl      =(60 * 1000); 
     const expires  = Date.now() + ttl;
     const data     = `${email}.${otp}.${expires}`;
-    const hash     = crypto.createHmac("sha256",process.env.key).update(data).digest("hex"); // creating SHA256 hash of the data
+    const hash     = crypto.createHmac("sha256",process.env.key).update(data).digest("hex"); // creating SHA256 hash of the data // Number representations "hex"
     const token = `${hash}.${expires}`; 
     return { token,otp};
 }
 
 
+
 router.post("/",loginAccountLimiter, async (req,res,next)=>{
 
-    
+    // request user details
     let user = await req.body
     const  trackId=Math.floor(new Date())
     const timestamp=new Date(),
      email = (user.email || '').trim()
     if (email.length===0) {
         return res.status(401).send({trackId,statusCode:400,timestamp,message:'Email not provided',path:__filename})
-        // throw new Error("Email i salready taken")
+        // throw new Error("Email not provided")
         }
     const valid= Isemail.validate(email)
     if(!valid){
         return res.status(400).send({trackId,statusCode:400,timestamp,message:"Enter valid Email",path:__filename})
     }
     try{
-
-
+        // otp and token generation request
         const {token, otp}=await createNewOTP(email)
         res.cookie("jwttoken",token,{
             expires:new Date(Date.now()+(1000*60)),
@@ -51,63 +60,17 @@ router.post("/",loginAccountLimiter, async (req,res,next)=>{
         })
     
 
-
-        // const mailOptions = {
-        //     from: '"Manoj kumar Admin 👻" <admin@manojkumar.com>', // sender address
-        //     to: user.email, // list of receivers
-        //     subject: "your Otp is successfully send ✔ ", // Subject line
-        //     text: `Hello sir/madam your otp is ${otp}`, // plain text body
-        //     html: `<b>Hello sir/madam your otp is ${otp}</b>`, // html body
-        // }
-        // {
-        //     from: `"Manoj kumar"<${process.env.EMAIL_ADDRESS}>`,
-        //     to: `${email}`,
-        //     subject: email_subject,
-        //     text: email_message ,
-        //   };
-
-
-        // await transporter.sendMail(mailOptions, async (err, res) => {
-        //     if (err) {
-        //         return res.status(400).send({"Status":"Failure","Details": err });
-        //     } else {
-        //     //   return res.send({"Status":"Success","Details":encoded});
-        //     user= await Generate.create(req.body);
-        //       return res.status(201).send({user,token,message:"OTP  successfully  send in your Email, it will expire in 5 minutes"})
-        //     }
-        //   });
-
-        
-        //    const otp= Math.random().toString().substring(2, 8);
-        //   const generate=await Generate.create(req.body);
-        // email send 
-        // transporter.sendMail({
-        //     from: '"Manojgum Admin 👻" <admin@manojkumar.com>', // sender address
-        //     to: user.email, // list of receivers
-        //     subject: "your Otp is successfully send ✔ ", // Subject line
-        //     text: `Hello sir/madam your otp is ${otp}`, // plain text body
-        //     html: `<b>Hello sir/madam your otp is ${otp}</b>`, // html body
-        // });
-        // return res.status(201).send({message: "OTP  successfully  send in your Email "})
-        // user= await User.create(req.body);
-        
-        // user= await Generate.create(req.body);
-       const details={
-            "timestamp":timestamp, 
-            "TraceID":trackId,
-            "Email": email,
-            "token": token,
-            "success": true,
-            "statusCode":200,
-            "message":"OTP  successfully  send in your Email, it will expire in 5 minutes",
-            "otp":otp
-            
-        };
-       
-        return res.status(200).json(details)
-        // return res.status(201).send({user,token,message:"OTP  successfully  send in your Email, it will expire in 5 minutes"})
+        // Email sends to the user 
+        transporter.sendMail({
+            from: '"Manojkumar Admin 👻" <admin@gmail.com>', // sender address
+            to: user.email, // list of receivers
+            subject: "your Otp is successfully send ✔ ", // Subject line
+            text: `Hello sir/madam your otp is ${otp}`, // plain text body
+            html: `<b>Hello sir/madam your otp is ${otp}</b>`, // html body
+        });
+        return res.status(201).send({message: "OTP  successfully  send in your Email, it will expire in 1 minutes "})
+     
     }catch(err){
-    const  trackId=Math.floor(new Date())
         return res.status(500).send({trackId,statusCode:500,timestamp,message:err.message,path:__filename})
     }
 })
