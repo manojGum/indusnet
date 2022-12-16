3f80f0b4d2ad16169fca845dc067231593fb6ae8
manoj kumar <Manojgum@gmail.com>
2022-12-16 13:14:13 +0530

    deleted duplicate  folder

--- Totpservice - Copy/src/configs/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-// database connection
-const connect = ()=>{
-    return mongoose.connect("mongodb://127.0.0.1:27017/pagination");
-};
-
-module.exports=connect;
