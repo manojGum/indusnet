ebb1c23c575380a537e61723763b374a5606dec9
manoj kumar <Manojgum@gmail.com>
2023-03-17 11:21:34 +0530

    space remove

--- Totpservice/src/middlewares/authenticate.js
+++ Totpservice/src/middlewares/authenticate.js
@@ -1,9 +1,10 @@
 const authenticate = async (req, res, next) => {
+  /*
   // if(!req.headers.authorization)
   // return res.status(400).send({message:"Authorization token not found or incorrect"});
-
   // if(!req.headers.authorization.startsWith("Bearer"))
   // return res.status(400).send({message:"Authorization token not found or incorrect"});
+   */
 
   // const token=req.headers.authorization.trim().split(" ")[1]
   // middle ware to check token avilabe or not if
